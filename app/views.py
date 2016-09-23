7c1c1136594852c33efb3332cc1d36d3e0f89637
Matthias Fulz <mfulz@olznet.de>
2016-09-23 15:52:47 +0200

    Added login form based on uikit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from app import app, db
 from app.models import User, Domain
-from flask import request, abort, render_template
-from flask_login import logout_user, login_required
+from flask import request, abort, render_template, redirect
+from flask_login import login_user, logout_user, login_required
 from .forms.login import LoginForm
 from .login.user import load_user
 
@@ -9,6 +9,35 @@ from .login.user import load_user
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            # login needed
+            print "OK"
+            login_user(user)
+            return redirect('/get_user?username={0}'.format(user.username))
+        else:
+            abort(401)
     return render_template('login.html',
-                           title='Sign In',
-                           form=form)
+                            title='Sign In',
+                            form=form)
+
+@app.route('/add_user', methods=['GET'])
+@login_required
+def add_user():
+    user = User(username=request.args.get("username"))
+    db.session.add(user)
+    db.session.commit()
+    return "OK"
+    
+@app.route('/get_user', methods=['GET'])
+@login_required
+def get_user():
+    user = User.query.filter_by(username=request.args.get("username")).first()
+    return str(user.id)
+    
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login')
